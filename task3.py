f62a38f267b792c36b57834bab24e39fb9fee7cd
abelkk <abelkemb@gmail.com>
2026-06-21 22:45:55 +0300

    updated

--- task3.py
+++ task3.py
@@ -3,8 +3,4 @@ if word == word[::-1]:
     print("The word is a palindrome.")
 else:
     print("The word is not a palindrome.")
-    for a,e,i,o,u in word:
-        if (a,e,i,o,u) in word:
-            print("word contains a vowel")
-        else:
-            print("doesn't have a vowel")
+    
